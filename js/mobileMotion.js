5724aacd6eb705363ea016bd3357e1265569be3b
Clément LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-08-11 11:42:06 +0200

    💄 adaptation responsive en hauteur

--- js/mobileMotion.js
+++ js/mobileMotion.js
@@ -1,3 +1,6 @@
+import gsap from "https://cdn.skypack.dev/gsap@3.6.1";
+import ScrollTrigger from "https://cdn.skypack.dev/gsap@3.6.1/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 const ballListener = document.querySelector(".ball");
 const {
   x: ballX,
@@ -23,7 +26,40 @@ if (window.DeviceOrientationEvent) {
 const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
   // do something amazing
   //rotate ball on x and y axis
-  ballListener.style.transform = `rotateX(${frontToBack * 2}deg) rotateY(${
-    -leftToRight * 2
-  }deg)`;
+  ballListener.style.transform = `rotateX(${frontToBack * 2}deg) rotateY(${-leftToRight * 2
+    }deg)`;
 };
+
+var text = document.querySelector(".separator div p");
+
+gsap.to(text, {
+  duration: 4,
+  repeat: -1,
+
+  x: -innerWidth,
+  ease: "none",
+
+})
+
+
+var text2 = document.querySelector(".separator2 div p");
+
+
+
+gsap.to(text2, {
+  duration: 4,
+  repeat: -1,
+  x: -innerWidth,
+  ease: "ease",
+})
+
+var text3 = document.querySelector(".separator3 div p");
+
+
+
+gsap.to(text3, {
+  duration: 4,
+  repeat: -1,
+  x: -innerWidth,
+  ease: "none",
+})
